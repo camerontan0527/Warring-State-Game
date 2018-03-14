38f15f4dc7f7a30d43642b96a521c4db8bd39e21
harma <u6381216@anu.edu.au>
2018-03-14 21:53:45 +1100

    updated comments in task 3

--- src/comp1110/ass2/WarringStatesGame.java
+++ src/comp1110/ass2/WarringStatesGame.java
@@ -57,7 +57,7 @@ public class WarringStatesGame {
     static boolean isPlacementWellFormed(String placement) {
         // FIXME Task 3: determine whether a placement is well-formed
         //first two char of string = card
-        //third char of string = location 
+        //third char of string = location
         //need to check if all cards are placed on the board once
         //need to check if the locations are all different (0-9 numbers no repeats)
         //can use if statements
